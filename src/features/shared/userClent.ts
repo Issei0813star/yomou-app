3e64af0ff3fec6d3106006c31a835bb4fcd45487
Issei0813star <issei0813star.dev@gmail.com>
2025-08-08 02:21:11 +0900

    API分離

--- src/features/shared/userClent.ts
+++
@@ -1,6 +0,0 @@
-import * as service from "../users/server/service/userService";
-
-export const userClient = {
-  getByEmail: service.getByEmail,
-  createUser: service.createUser,
-};
--- src/features/signup/server/api/handler.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest } from "next/server";
-import { doSignup } from "../usecase/signup";
-
-export async function signup(req: NextRequest) {
-  const { email, username, password } = await req.json();
-  await doSignup(email, username, password);
-}
